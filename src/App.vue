9124d9f0c3ae2047acf81805ea3499411383e499
pvogel <patrik.vogel@paessler.com>
2021-02-11 12:34:25 +0100

    Add methods and event binding

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <p>{{ productName }}</p>
   <p>The <a v-bind:href="vueLink">Vue.js docs</a> are great.</p>
+  <button v-on:click="increment">Increment</button>
+  <p>{{ counter }}</p>
 </template>
 
 <script>
@@ -10,7 +12,13 @@ export default {
     return {
       productName: "prtg",
       vueLink: "https://v3.vuejs.org/guide/introduction.html",
+      counter: null,
     };
   },
+  methods: {
+    increment(){
+      this.counter++;
+    }
+  }
 };
 </script>
