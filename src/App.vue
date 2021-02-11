4d96ce54c99202aee0d8cb1cba004722432dff0a
pvogel <patrik.vogel@paessler.com>
2021-02-11 16:04:14 +0100

    Add computed properties and watcher

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
   <p>The <a v-bind:href="vueLink">Vue.js docs</a> are great.</p>
   <button v-on:click.alt="increment">Increment</button>
   <p>{{ counter }}</p>
-  <input type="text" v-model="productName" />
+  <label><input type="text" v-model="productName"/></label>
   <button v-on:click="resetInput">Reset</button>
   <form v-on:submit.prevent="submitForm">
-    <input type="text" />
+    <label><input type="text"/></label>
     <button>Sign Up</button>
   </form>
+  <!--  <p>Some Name: {{ productName + " " + "XL5" }}</p>-->
+  <p>Some Name {{ someName }}</p>
 </template>
 
 <script>
@@ -31,6 +33,21 @@ export default {
     },
     resetInput() {
       this.productName = "";
+    },
+  },
+  computed: {
+    someName() {
+      return this.productName + " " + "XL5";
+    },
+  },
+  watch: {
+    counter(value) {
+      if (value > 4) {
+        const that = this; // arrow function to the rescue
+        setTimeout(function() {
+          that.counter = 0;
+        }, 2000);
+      }
     },
   },
 };
