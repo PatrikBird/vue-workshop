00ed8426049d67b03e46facd04a77a8cd683d5ca
pvogel <patrik.vogel@paessler.com>
2021-02-11 12:25:07 +0100

    Add data property, interpolation and v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-    <!-- ... -->
-  </div>
+  <p>{{ productName }}</p>
+  <p>The <a v-bind:href="vueLink">Vue.js docs</a> are great.</p>
 </template>
 
 <script>
 export default {
   name: "App",
-  // ...
+  data() {  // data: function()
+    return {
+      productName: "prtg",
+      vueLink: "https://v3.vuejs.org/guide/introduction.html",
+    };
+  },
 };
 </script>
-
-<style>
-/* ... */
-</style>
