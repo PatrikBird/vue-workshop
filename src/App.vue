fc8016b07d6d0ff0053f76cdefa2486b39584364
pvogel <patrik.vogel@paessler.com>
2021-02-11 13:10:32 +0100

    Add event modifier

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,45 @@
 <template>
   <p>{{ productName }}</p>
   <p>The <a v-bind:href="vueLink">Vue.js docs</a> are great.</p>
-  <button v-on:click="increment">Increment</button>
+  <button v-on:click.alt="increment">Increment</button>
   <p>{{ counter }}</p>
+  <input
+    type="text"
+    v-on:input="setProductName"
+    v-on:keyup.enter="SetConfirmedProductName"
+    v-on:blur="SetConfirmedProductName"
+  />
+  <p>{{ confirmedProductName }}</p>
+  <form v-on:submit.prevent="submitForm">
+    <input type="text" />
+    <button>Sign Up</button>
+  </form>
 </template>
 
 <script>
 export default {
   name: "App",
-  data() {  // data: function()
+  data() {
     return {
       productName: "prtg",
+      confirmedProductName: "",
       vueLink: "https://v3.vuejs.org/guide/introduction.html",
       counter: null,
     };
   },
   methods: {
-    increment(){
+    increment() {
       this.counter++;
-    }
-  }
+    },
+    setProductName(event) {
+      this.productName = event.target.value;
+    },
+    submitForm() {
+      alert("submitted");
+    },
+    SetConfirmedProductName() {
+      this.confirmedProductName = this.productName;
+    },
+  },
 };
 </script>
