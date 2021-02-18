75c6a1eca2b540a5f35da7a156f5fe6479b10511
Patrik Vogel <patrik.vogel@paessler.com>
2021-02-18 14:58:46 +0100

    Increase overall readability

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <p>{{ productName }}</p>
-  <p>The <a v-bind:href="vueLink">Vue.js docs</a> are great.</p>
-  <button v-on:click.alt="increment">Increment</button>
-  <p>{{ counter }}</p>
-  <label><input type="text" v-model="productName"/></label>
-  <button v-on:click="resetInput">Reset</button>
-  <form v-on:submit.prevent="submitForm">
-    <label><input type="text"/></label>
-    <button>Sign Up</button>
-  </form>
-  <!--  <p>Some Name: {{ productName + " " + "XL5" }}</p>-->
-  <p>Some Name {{ someName }}</p>
+  <section id="events">
+    <p>The <a v-bind:href="vueLink">Vue.js docs</a> are great.</p>
+    <button v-on:click.left="increment">Increment</button>
+    <p>Counter: {{ counter }}</p>
+    <p>Product Name: {{ productName }}</p>
+    <label><input type="text" v-model="productName"/></label>
+    <button v-on:click="resetInput">Reset</button>
+    <form v-on:submit.prevent="submitForm">
+      <label><input type="text"/></label>
+      <button>Sign Up</button>
+    </form>
+    <!--  <p>Some Name: {{ productName + " " + "XL5" }}</p>-->
+    <p>Some Name: {{ someName }}</p>
+  </section>
 </template>
 
 <script>
@@ -19,7 +21,6 @@ export default {
   data() {
     return {
       productName: "prtg",
-      confirmedProductName: "",
       vueLink: "https://v3.vuejs.org/guide/introduction.html",
       counter: null,
     };
